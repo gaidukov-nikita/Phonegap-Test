34b760733ca025d6a15780340643cf75f441ebd2
gaydukov-nikita <gaydukov.nikita@gmail.com>
2015-09-08 11:19:21 +0300

    13

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,46 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/jaeger.Html5Video/www/Html5Video.js",
+        "id": "jaeger.Html5Video.Html5Video",
+        "clobbers": [
+            "plugins.html5Video"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-dialogs": "1.1.2-dev",
+    "cordova-plugin-whitelist": "1.0.0",
+    "jaeger.Html5Video": "1.2.1",
+    "cordova-plugin-device": "1.0.2-dev"
+}
+// BOTTOM OF METADATA
+});
