56abd1b896cfa7d6cba58f69295de8b6210b5dcd
gaydukov-nikita <gaydukov.nikita@gmail.com>
2015-09-08 11:33:05 +0300

    15

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
         "id": "cordova-plugin-dialogs.notification",
         "merges": [
@@ -25,22 +32,15 @@ module.exports = [
         "clobbers": [
             "plugins.html5Video"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "clobbers": [
-            "device"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-device": "1.0.2-dev",
     "cordova-plugin-dialogs": "1.1.2-dev",
     "cordova-plugin-whitelist": "1.0.0",
-    "jaeger.Html5Video": "1.2.1",
-    "cordova-plugin-device": "1.0.2-dev"
+    "jaeger.Html5Video": "1.2.1"
 }
 // BOTTOM OF METADATA
 });
